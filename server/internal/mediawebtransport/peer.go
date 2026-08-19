0e5a9ef1e803c89d5f62365252a5cfffcd4e9b83
a <ash@bashdev.org>
2026-08-19 15:35:49 -0500

    perf: reuse WebTransport sample buffers

--- server/internal/mediawebtransport/peer.go
+++ server/internal/mediawebtransport/peer.go
@@ -19,7 +19,7 @@ type peer struct {
 	session   *webtransport.Session
 	sessionID string
 
-	samples   chan []byte
+	samples   chan *media.EncodedSample
 	done      chan struct{}
 	closeOnce sync.Once
 	wg        sync.WaitGroup
@@ -30,7 +30,7 @@ func newPeer(manager *Manager, session *webtransport.Session, sessionID string) 
 		manager:   manager,
 		session:   session,
 		sessionID: sessionID,
-		samples:   make(chan []byte, 64),
+		samples:   make(chan *media.EncodedSample, 64),
 		done:      make(chan struct{}),
 	}
 }
@@ -43,6 +43,7 @@ func (peer *peer) WriteSample(track byte, sample types.Sample) {
 	message := media.EncodeSample(track, sample)
 	select {
 	case <-peer.done:
+		message.Release()
 		return
 	default:
 	}
@@ -50,6 +51,7 @@ func (peer *peer) WriteSample(track byte, sample types.Sample) {
 	select {
 	case peer.samples <- message:
 	default:
+		message.Release()
 		peer.Close(errors.New("media webtransport client is too slow"))
 	}
 }
@@ -78,28 +80,43 @@ func (peer *peer) Run() {
 
 func (peer *peer) write() {
 	defer peer.wg.Done()
+	defer peer.releaseSamples()
 	for {
 		select {
 		case <-peer.done:
 			return
 		case sample := <-peer.samples:
-			ctx, cancel := context.WithTimeout(peer.session.Context(), 5*time.Second)
-			stream, err := peer.session.OpenUniStreamSync(ctx)
-			cancel()
-			if err != nil {
+			if err := peer.writeSample(sample); err != nil {
 				peer.Close(err)
 				return
 			}
-			_ = stream.SetWriteDeadline(time.Now().Add(5 * time.Second))
-			if _, err := io.Copy(stream, bytes.NewReader(sample)); err != nil {
-				stream.CancelWrite(0)
-				peer.Close(err)
-				return
-			}
-			if err := stream.Close(); err != nil {
-				peer.Close(err)
-				return
-			}
+		}
+	}
+}
+
+func (peer *peer) writeSample(sample *media.EncodedSample) error {
+	defer sample.Release()
+	ctx, cancel := context.WithTimeout(peer.session.Context(), 5*time.Second)
+	stream, err := peer.session.OpenUniStreamSync(ctx)
+	cancel()
+	if err != nil {
+		return err
+	}
+	_ = stream.SetWriteDeadline(time.Now().Add(5 * time.Second))
+	if _, err := io.Copy(stream, bytes.NewReader(sample.Data)); err != nil {
+		stream.CancelWrite(0)
+		return err
+	}
+	return stream.Close()
+}
+
+func (peer *peer) releaseSamples() {
+	for {
+		select {
+		case sample := <-peer.samples:
+			sample.Release()
+		default:
+			return
 		}
 	}
 }
