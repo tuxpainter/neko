c47ab3467ca1fc90b5d59b8000f279772ddc1235
a <ash@bashdev.org>
2026-08-19 15:31:26 -0500

    fix: serve WebTransport dev client over HTTPS

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,4 +1,8 @@
 const path = require('path')
+const fs = require('fs')
+
+const tlsCert = process.env.NEKO_CLIENT_TLS_CERT
+const tlsKey = process.env.NEKO_CLIENT_TLS_KEY
 
 module.exports = {
   productionSourceMap: false,
@@ -23,5 +27,16 @@ module.exports = {
   },
   devServer: {
     allowedHosts: 'all',
+    ...(tlsCert && tlsKey
+      ? {
+          server: {
+            type: 'https',
+            options: {
+              cert: fs.readFileSync(tlsCert),
+              key: fs.readFileSync(tlsKey),
+            },
+          },
+        }
+      : {}),
   },
 }
